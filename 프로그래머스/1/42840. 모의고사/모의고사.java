521fb18d9375c102feeb6bda53eb0f59fbed6bf9
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2023-12-10 19:20:12 +0900

    [level 1] Title: 모의고사, Time: 3.89 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        int[] score = {0,0,0};
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == one[i % one.length]) {
+                score[0]++;
+            }
+            if (answers[i] == two[i % two.length]) {
+                score[1]++;
+            }
+            if (answers[i] == three[i % three.length]) {
+                score[2]++;
+            }
+        }
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (maxScore == score[i]) {
+                result.add(i + 1);
+            }
+        }
+        return result.stream().mapToInt(o -> o).toArray();
+    }
+}
