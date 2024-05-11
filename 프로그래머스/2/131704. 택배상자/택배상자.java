e4cfca12de4d72d333d8c38a48e16c3832b72728
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-05-11 21:22:30 +0900

    [level 2] Title: 택배상자, Time: 50.03 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        int[] priority = new int[order.length];
+        for (int i = 0; i < order.length; i++) {
+            priority[order[i] - 1] = i;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < order.length; i++) {
+            if (priority[i] == answer) {
+                answer++;
+            }
+            else {
+                stack.push(priority[i]);
+            }
+            while(!stack.isEmpty() && stack.peek() == answer) {
+                stack.pop();
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
