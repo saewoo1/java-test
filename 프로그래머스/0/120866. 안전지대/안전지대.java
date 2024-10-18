9359eae62a3a9f18dcc54db6731d470d442875ae
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-10-18 15:53:54 +0900

    [level 0] Title: 안전지대, Time: 0.18 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 class Solution {
-    
-    public int[][] makeDangerMark(int row, int col, int[][] dir, int[][] board) {
-        for (int[] d : dir) {
-            int newRow = row + d[0];
-            int newCol = col + d[1];
-            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && board[newRow][newCol] == 0) {
-                board[newRow][newCol] = 3;
-            }
-        }
-        return board;
-    }
-    
     public int solution(int[][] board) {
+        Queue<int[]> minePos = new LinkedList<>();
         int answer = 0;
         int[][] dir = {{1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+    
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 1) {
-                    board = makeDangerMark(i, j, dir, board);
+                    minePos.offer(new int[]{i, j});
+                }
+            }
+        }
+        while (!minePos.isEmpty()) {
+            int[] pos = minePos.poll();
+            for (int[] d : dir) {
+                int newRow = pos[0] + d[0];
+                int newCol = pos[1] + d[1];
+                if ((newRow >= 0 && newRow < board.length) && (newCol >= 0 && newCol < board[0].length) && board[newRow][newCol] == 0) {
+                    board[newRow][newCol] = 2;
                 }
             }
         }
