1341979514d24b17867263c8dc1b50337c941c96
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-10-19 00:40:36 +0900

    [level 0] Title: 암호 해독, Time: 0.21 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder answer = new StringBuilder();
+        char[] cipherChars = cipher.toCharArray();
+        for (int i = code - 1; i < cipher.length(); i += code) {
+            answer.append(cipher.charAt(i));
+        }
+        return answer.toString();
+    }
+}
