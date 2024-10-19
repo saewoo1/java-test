cfc5d988e431f6251d092cd9469b33c0b35bb645
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-10-19 22:09:23 +0900

    [level 0] Title: A로 B 만들기, Time: 1.41 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -2,11 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String before, String after) {
-        char[] str1 = before.toCharArray();
-        char[] str2 = after.toCharArray();
+        Map<Character, Integer> beforeCounter = getCharCountMap(before);
+        Map<Character, Integer> afterCounter = getCharCountMap(after);
         
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-        return new String(str1).contains(new String(str2)) ? 1 : 0;
+        for (Map.Entry<Character, Integer> entry : beforeCounter.entrySet()) {
+            char key = entry.getKey();
+            int value = entry.getValue();
+            
+            if (!afterCounter.containsKey(key) || afterCounter.get(key) < value) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    private Map<Character, Integer> getCharCountMap(String str) {
+        Map<Character, Integer> counter = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
+        }
+        
+        return counter;
     }
 }
