584b7a42fc53a207272854a05eea7aede720efe5
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-10-20 17:13:16 +0900

    [level 0] Title: 소인수분해, Time: 4.73 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,29 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        if (n < 2) return new int[0];
-        Set<Integer> set = new HashSet<>();
-        for (int i = 2; i <= n; i++) {
-            if (n % i == 0 && isPrime(i)) {
-                set.add(i);
-            }
-        }
-        int[] answer = set.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
-        
-        Arrays.sort(answer);
-        return answer;
-    }
-    public boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        int i = 2;
+        while (n != 0 && i <= n) {
             if (n % i == 0) {
-                return false;
+                set.add(i);
+                n /= i;
+            }
+            else {
+                i++;
             }
         }
-        return true;
+        return set.stream().mapToInt(e -> e).toArray();
     }
 }
