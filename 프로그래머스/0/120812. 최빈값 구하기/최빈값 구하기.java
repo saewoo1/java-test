693a94bbe3122e101ad8555ec60348a722726f59
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-10-24 18:44:29 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,28 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] array) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        int maxCount = 0;
-        // 카운터 생성
-        for (int i = 0; i < array.length; i++) {
-            counter.put(array[i], counter.getOrDefault(array[i], 0) + 1);
-        }
-        List<Integer> keys = new ArrayList<>();
-        // maxCount 값 찾기
-        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-            int key = entry.getKey();
-            int value = entry.getValue();
-            
-            if (value > maxCount) {
-                keys.clear();
-                maxCount = value;
-                keys.add(key);
-            }
-            else if (value == maxCount) {
-                keys.add(key);
-            }
-        }
-        return keys.size() > 1 ? -1 : keys.get(0);
+        int answer = 0;
+        return answer;
     }
 }
