046b2663879a78590be62e0a3242f2a973fe461f
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-10-31 20:56:47 +0900

    [level 1] Title: K번째수, Time: 0.53 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int n = 0;
-        for (int i = 0; i < answer.length; i++) {
-            int[] list = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int target = commands[i][2];
             
-            Arrays.sort(list);
-            answer[n++] = list[commands[i][2] - 1];
+            int[] arrayInRange = Arrays.copyOfRange(array, start - 1, end);
+            Arrays.sort(arrayInRange);
+            answer[i] = arrayInRange[target - 1];
         }
         return answer;
     }
