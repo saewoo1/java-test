5fb6560f0c4fefef2e1ac2a66ecab843c4165890
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-07 02:32:14 +0900

    [level 1] Title: 2016년, Time: 1.41 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,6 +1,12 @@
+import java.util.*;
 import java.time.*;
+
 class Solution {
     public String solution(int a, int b) {
-        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
+        String answer = "";
+        int year = 2016;
+        LocalDate date = LocalDate.of(year, a, b);
+        
+        return date.getDayOfWeek().toString().substring(0, 3);
     }
 }
