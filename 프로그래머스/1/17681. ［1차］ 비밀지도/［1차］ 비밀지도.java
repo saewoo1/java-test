ef3a0318caed2115929e5605628e8cbdbd435b47
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-07 20:47:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.10 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,31 @@
+import java.util.*;
+
 class Solution {
+    
+    public int[] toBinary(int number, int size) {
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = number % 2;
+            number /= 2;
+        }
+        return result;
+    }
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for (int i = 0; i < n; i++) {
-            // 배열의 숫자들을 이진수로 변경
-            // 비트연산으로 or 수행 -> 하나라도 1이면 1로 저장
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            answer[i] = answer[i].replaceAll("0", " ");
-            answer[i] = answer[i].replaceAll("1", "#");
+            int[] line1 = toBinary(arr1[i], n);
+            int[] line2 = toBinary(arr2[i], n);
             
-            // 이진법으로 표현한 수가 n보다 짧다면 모두 공백으로 채워준다
-            while (answer[i].length() < n) {
-                answer[i] = " " + answer[i];
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < line1.length; j++) {
+                if (line1[j] == 0 && line2[j] == 0) {
+                    sb.append(' ');
+                }
+                else {
+                    sb.append('#');
+                }
             }
+            answer[i] = sb.reverse().toString();
         }
         return answer;
     }
