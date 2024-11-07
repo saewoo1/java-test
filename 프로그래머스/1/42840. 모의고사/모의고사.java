8e6dfb4b46d7620e4f94d2200d4ed5a85ff7e8ad
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-07 18:01:42 +0900

    [level 1] Title: 모의고사, Time: 3.81 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,35 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] three = {3,3,1,1,2,2,4,4,5,5};
-        int[] score = {0,0,0};
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        List<Integer> result = new ArrayList<>();
+        int[] scores = new int[3];
         
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == one[i % one.length]) {
-                score[0]++;
+            if (answers[i] == first[i % first.length]) {
+                scores[0]++;
             }
-            if (answers[i] == two[i % two.length]) {
-                score[1]++;
+            if (answers[i] == second[i % second.length]) {
+                scores[1]++;
             }
-            if (answers[i] == three[i % three.length]) {
-                score[2]++;
+            if (answers[i] == third[i % third.length]) {
+                scores[2]++;
             }
         }
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            if (maxScore == score[i]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < scores.length; i++) {
+            max = Math.max(scores[i], max);
+        }
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == max) {
                 result.add(i + 1);
             }
         }
-        return result.stream().mapToInt(o -> o).toArray();
+        
+        
+        return result.stream().mapToInt(e -> e).toArray();
     }
 }
