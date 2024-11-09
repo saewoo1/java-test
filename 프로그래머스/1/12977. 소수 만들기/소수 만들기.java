68e905ee53f5c3753c16affad6f932921ab82165
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-09 13:43:59 +0900

    [level 1] Title: 소수 만들기, Time: 1.98 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isSosu(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isSosu(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
