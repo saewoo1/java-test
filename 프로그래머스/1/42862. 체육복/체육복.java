c0d7e31ac81ebe3ce50529ef5c05fcf5fd8c0710
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-12 01:16:05 +0900

    [level 1] Title: 체육복, Time: 8.99 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,34 +1,31 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        List<Integer> losts = Arrays.stream(lost)
+            .filter(l -> Arrays.stream(reserve).noneMatch(r -> r == l))
+            .boxed()
+            .collect(Collectors.toList());
+        List<Integer> reserves = Arrays.stream(reserve)
+            .filter(r -> Arrays.stream(lost).noneMatch(l -> l == r))
+            .boxed()
+            .collect(Collectors.toList());
+        Collections.sort(losts);
+        Collections.sort(reserves);
         
-        /*
-        총 학생 수 - 잃어버린 학생 수
-        하지만 여분이 있는 학생 수가 존재하기 때문에 이를 계산해야한다.
-        */
-        int answer = n - lost.length;
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    answer++;
-                    reserve[j] = -1; // 못빌려줌. 내가써야됨
-                    lost[i] = -1; // 빌릴 필요도 없음
-                    break;
-                }
+        int answer = n - losts.size();
+        for (int reserver : reserves) {
+            if (losts.contains(reserver - 1)) {
+                answer++;
+                losts.remove(Integer.valueOf(reserver - 1));
+            }
+            else if (losts.contains(reserver + 1)) {
+                answer++;
+                losts.remove(Integer.valueOf(reserver + 1));
             }
         }
         
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (reserve[j] - 1 == lost[i] || reserve[j] + 1 == lost[i]) {
-                    answer++; // 빌려줌
-                    reserve[j] = -1;
-                    break;
-                }
-            }
-        }
         return answer;
     }
 }
