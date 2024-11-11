e91c5064a7e883345a1c6bc403f0761247f5757c
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-12 00:22:50 +0900

    [level 1] Title: 덧칠하기, Time: 3.29 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,18 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        int start = section[0];
-        answer++;
-        
-        for (int area : section) {
-            if (start + m > area) {
-                continue;
-            }
-            
-            start = area;
-            answer++;
+        int answer = 1;
+        if (m == 1) {
+            return section.length;
         }
-        
+        Arrays.sort(section);
+        int start = section[0];
+        for (int i = 1; i < section.length; i++) {
+            if (section[i] > start + m - 1) {
+                start = section[i];
+                answer++;
+            }
+        }
         return answer;
     }
 }
