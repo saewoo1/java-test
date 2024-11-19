dc0b29efa82e251ddb09f5f5e97a4972fe16b0a6
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-19 11:18:22 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        // 총 면적
         int area = brown + yellow;
-        // 가로, 세로는 최소 3부터 시작한다(노랑을 포함해야함)
-        int h = 3;
-        int w = 3;
-        while (true) {
-            // 나누어 떨어지는가?
-            if (area % h != 0) {
-                h++;
-                continue;
+        for (int brownW = 3; brownW < brown; brownW++) {
+            int brownH = area / brownW;
+            
+            if ((brownW - 2) * (brownH - 2) == yellow) {
+                return new int[]{brownH, brownW};
             }
-            w = area / h;
-            if ((h - 2) * (w - 2) == yellow) {
-                return new int[]{w, h};
-            }
-            h++;
         }
+        return new int[]{0,0};
     }
 }
