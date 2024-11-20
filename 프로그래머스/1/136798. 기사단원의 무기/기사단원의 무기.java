78301d5811c53775644deb4948b1173422416fb7
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-20 15:12:38 +0900

    [level 1] Title: 기사단원의 무기, Time: 14.16 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -4,21 +4,17 @@ class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
         int[] weapons = new int[number + 1];
-        
         for (int i = 1; i <= number; i++) {
             for (int j = i; j <= number; j += i) {
                 weapons[j]++;
             }
         }
-        
-        for (int i = 1; i <= number; i++) {
-            int measure = weapons[i];
-            if (measure > limit) {
-                measure = power;
+        for (int i = 0; i < weapons.length; i++) {
+            if (weapons[i] > limit) {
+                weapons[i] = power;
             }
-            answer += measure;
         }
-        return answer;
+        
+        return Arrays.stream(weapons).sum();
     }
-    
 }
