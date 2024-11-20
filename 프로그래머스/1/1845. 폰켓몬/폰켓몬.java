ac60a4632ce0531fcd02a2aaa6b5a6caebfd5ecc
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-20 16:52:56 +0900

    [level 1] Title: 폰켓몬, Time: 8.21 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = nums.length / 2;
+        int answer = 0;
+        int target = nums.length / 2;
         Map<Integer, Integer> counter = new HashMap<>();
         for (int num : nums) {
             counter.put(num, counter.getOrDefault(num, 0) + 1);
         }
-        if (counter.size() < answer) {
-            answer = counter.size();
+        if (counter.size() >= target) {
+            return target;
+        }
+        if (counter.size() < target) {
+            return counter.size();
         }
         return answer;
     }
