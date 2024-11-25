33a7642974828a1bf6ab94f4acc24af9a6a4c635
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-25 12:14:31 +0900

    [Silver III] Title: 피보나치 함수, Time: 196 ms, Memory: 18392 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,29 +1,35 @@
 import java.util.*;
 // N은 0 ~ 40, 0과 1은 각각 몇 번씩 호출되었을까?
 public class Main {
-    static int[] zeroCount;
-    static int[] oneCount;
+    static int[] fibo;
     static int N;
     static int T;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         T = scanner.nextInt();
-        zeroCount = new int[45];
-        zeroCount[0] = 1;
-        zeroCount[1] = 0;
+        fibo = new int[45];
+        fibo[0] = 0;
+        fibo[1] = 1;
         
-        oneCount = new int[45];
-        oneCount[1] = 1;
-        oneCount[0] = 0;
-        
+        // dp에 수들을 채워 넣는다
         for (int i = 2; i < 45; i++) {
-            zeroCount[i] = zeroCount[i - 2] + zeroCount[i - 1];
-            oneCount[i] = oneCount[i - 2] + oneCount[i - 1];
+            fibo[i] = fibo[i - 2] + fibo[i - 1];
         }
+        /* 
+        fibo[5] = fibo[3] + fibo[4]
+                = (fibo[1] + fibo[2]) + (fibo[2] + fibo[3])
+                = (fibo[1] + fibo[0] + fibo[1]) + (fibo[0] + fibo[1] + fibo[1] + fibo[2])
+                = (1 + 0 + 1) + (0 + 1 + 1 + fibo[0] + fibo[1])
+                = (1 = 5회, 0 = 3회)
         
+        */
         for (int i = 0; i < T; i++) {
             N = scanner.nextInt();
-            System.out.println(zeroCount[N] + " " + oneCount[N]);
+            if (N == 0) {
+                System.out.println("1 0");
+                continue;
+            }
+            System.out.println(fibo[N - 1] + " " + fibo[N]);
         }
     }
 }
