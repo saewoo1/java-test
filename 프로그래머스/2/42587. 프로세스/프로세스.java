715fafc8debc0dc843a5ad1b09b317e38a696fb0
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-27 11:34:04 +0900

    [level 2] Title: 프로세스, Time: 8.20 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,6 +1,38 @@
+import java.util.*;
+
 class Solution {
+    class Process {
+        int priority;
+        int location;
+        
+        public Process(int priority, int location) {
+            this.priority = priority;
+            this.location = location;
+        }
+    }
+    
     public int solution(int[] priorities, int location) {
         int answer = 0;
+        Queue<Process> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(new Process(priorities[i], i));
+        }
+        
+        while(!queue.isEmpty()) {
+            Process job = queue.poll();
+            if (queue.stream().
+                anyMatch(otherJob -> otherJob.priority > job.priority)) {
+                // 나머지 중 우선 순위가 더 높은 애가 있음
+                queue.offer(job);
+            }
+            else {
+                answer++;
+                if (job.location == location) {
+                    break;
+                }
+            }
+            
+        }
         return answer;
     }
 }
