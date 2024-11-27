0519d8526ef375390000092e8b21d1bc883619f2
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-11-27 13:19:20 +0900

    [level 2] Title: 기능개발, Time: 4.87 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,22 +1,41 @@
 import java.util.*;
 
 class Solution {
+    
+    class Process {
+        int start;
+        int speed;
+        
+        public Process(int start, int speed) {
+            this.start = start;
+            this.speed = speed;
+        }
+    }
+    
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
+        Queue<Process> queue = new LinkedList<>();
         
-        for (int i = 0; i < progresses.length; i++) {
-            double days = (100 - progresses[i]) / (double)speeds[i];
-            int day = (int) Math.ceil(days);
-            
-            int j = i + 1;
-            for (; j < progresses.length; j++) {
-               if (progresses[j] + day * speeds[j] < 100) {
-                   break;
-               }
-            }
-            answer.add(j - i);
-            i = j - 1;
+        // 큐에 각 정보를 담는다
+        for (int i = 0; i < speeds.length; i++) {
+            queue.offer(new Process(progresses[i], speeds[i]));
         }
+        
+        int day = 0;
+        while (!queue.isEmpty()) {
+            int count = 0;
+            
+            // 완료 가능한 작업이 있음
+            while (!queue.isEmpty() && queue.peek().start + queue.peek().speed * day >= 100) {
+                queue.poll();
+                count++;
+            }
+            if (count > 0) {
+                answer.add(count);
+            }
+            day++;
+        }
+        
         return answer.stream().mapToInt(e -> e).toArray();
     }
 }
