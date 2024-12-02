3fc6a452aa17bb2c0e06854ddd7a44198fc5ace9
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-12-02 19:18:16 +0900

    [level 3] Title: 여행경로, Time: 92.58 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,32 +1,12 @@
 import java.util.*;
 
 class Solution {
-    List<String> answer = new ArrayList<>();
-    boolean[] visited;
-    int n;
+    
+    static String[][]
     
     public String[] solution(String[][] tickets) {
+        List<String> answer = new ArrayList<>();
         
-        n = tickets.length;
-        visited = new boolean[n];
-        dfs(0, "ICN", "ICN", tickets);
-        Collections.sort(answer);
-        
-        return answer.get(0).split(" ");
-    }
-    
-    public void dfs(int depth, String start, String path, String[][] tickets) {
-        if (depth == n) {
-            answer.add(path);
-            return ;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i] && tickets[i][0].equals(start)) {
-                visited[i] = true;
-                dfs(depth + 1, tickets[i][1], path + " " + tickets[i][1], tickets);
-                visited[i] = false;
-            }
-        }
+        return answer;
     }
 }
