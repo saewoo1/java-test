f762eaefc9074381c403d26ea35750d6b1d805e5
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-12-14 13:45:09 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        if (my_string.startsWith(is_prefix)) {
+            answer =1;
+        }
+        return answer;
+    }
+}
