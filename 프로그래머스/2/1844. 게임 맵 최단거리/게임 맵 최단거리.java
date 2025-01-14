f3e2b5392efa77bb9dc418b64300abc26e9c70d2
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-01-14 20:03:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.94 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,22 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] maps) {
-        int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        
-        answer = bfs(visited, maps, n, m);
-        return answer;
-    }
+    boolean[][] visited;
+    int n;
+    int m;
     
-    public int bfs(boolean[][] visited, int[][] maps, int n, int m) {
+    public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
+        
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 0, 1});
         visited[0][0] = true;
-        int[] dirR = {1,-1,0,0};
-        int[] dirC = {0,0,1,-1};
+        
+        int[] dirR = {1, -1, 0, 0};
+        int[] dirC = {0, 0, 1, -1};
         
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -24,7 +23,6 @@ class Solution {
             int x = current[1];
             int distance = current[2];
             
-            // 끝에 도달했으면 distance 반환하기
             if (y == n - 1 && x == m - 1) {
                 return distance;
             }
@@ -33,13 +31,14 @@ class Solution {
                 int newY = dirR[i] + y;
                 int newX = dirC[i] + x;
                 
-                if (((newY >= 0 && newY < n) && (newX >= 0 && newX < m)) 
-                    && (!visited[newY][newX] && maps[newY][newX] == 1)) {
-                    visited[newY][newX] = true;
+                if ((newY >= 0 && newY < n) && (newX >= 0 && newX < m) 
+                   && (maps[newY][newX] == 1 && !visited[newY][newX])) {
                     queue.add(new int[]{newY, newX, distance + 1});
+                    visited[newY][newX] = true;
                 }
             }
         }
+        
         return -1;
     }
 }
