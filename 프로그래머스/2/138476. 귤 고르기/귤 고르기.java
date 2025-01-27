b37ac970e2a1c50ca63b9428a598c6c41a997bda
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-01-27 16:39:10 +0900

    [level 2] Title: 귤 고르기, Time: 63.70 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,27 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        List<Integer> count = new ArrayList<>();
-        Map<Integer, Integer> tangerineMap = new HashMap<>();
-        for (int tan : tangerine) {
-            tangerineMap.put(tan, tangerineMap.getOrDefault(tan, 0) + 1);
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (Integer gyul : tangerine) {
+            counter.put(gyul, counter.getOrDefault(gyul, 0) + 1);
         }
+        // counter 에서 value가 최소인 애들 먼저 빼고, k랑 같아진 시점에서 key의 개수?
+        List<Integer> sortedValues = new ArrayList<>(counter.values());
+        // 오름차순 정렬
+        Collections.sort(sortedValues, Collections.reverseOrder());
         
-        for (int tan : tangerineMap.values()) {
-            count.add(tan);
-        }
-        // 개수 많은거 먼저
-        Collections.sort(count, Collections.reverseOrder());
-        int totalCount = 0;
-        int totalSort = 0;
-        for (int value : count) {
-            totalSort++;
+        int total = 0; // 고른거 총 개수
+        int count = 0; // 고른 귤 종류 수
+        for (int value : sortedValues) {
+            count++;
             k -= value;
+            
             if (k <= 0) {
-                return totalSort;
+                return count;
             }
+           
         }
-        return answer;
+        return count;
     }
 }
