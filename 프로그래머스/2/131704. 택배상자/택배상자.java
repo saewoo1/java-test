b9f5b59b566830d11e6dc38c2c1724f75648412c
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-02-12 16:15:10 +0900

    [level 2] Title: 택배상자, Time: 191.25 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,14 +1,33 @@
 import java.util.*;
-
+// queue를 2개 두고?
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> belt = new LinkedList<>();
+        Stack<Integer> sub = new Stack<>();
+        // 벨트에 순차적으로 담긴 상자들
         for (int i = 1; i <= order.length; i++) {
-            stack.push(i);
-            while(!stack.isEmpty() && stack.peek() == order[answer]) {
-                stack.pop();
+            belt.offer(i);
+        }
+        
+        for (int boxOrder : order) {
+            // 보조벨트에서 찾음 -> answer 늘리고, 다음 박스 ㄱㄱ
+            if (!sub.isEmpty() && sub.peek() == boxOrder) {
+                sub.pop();
                 answer++;
+                continue;
+            }
+            
+            // 벨트에 맞는게 없으면 보조벨트로 옮긴다
+            while (!belt.isEmpty() && belt.peek() != boxOrder) {
+                sub.push(belt.poll());
+            }
+            
+            if (!belt.isEmpty() && belt.peek() == boxOrder) {
+                belt.poll();
+                answer++;
+            } else {
+                break;
             }
         }
         return answer;
