f5f3049a3c7062180d79f26bda91535e7b4d29e0
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-02-14 19:34:22 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
+        int answer = 0;
         // 서로 다른 옷의 조합 개수?
-        Map<String, Integer> closet = new HashMap<>();
+        Map<String, String> closet = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            String type = clothes[i][1];
-            String clothe = clothes[i][0];
-            
-            closet.put(type, closet.getOrDefault(type, 0) + 1);
-        }
-        for (String key : closet.keySet()) {
-            answer *= closet.get(key) + 1;
+            closet.put(clothes[i][0], clothes[i][1]);
         }
         
+        answer += closet.size();
         
-        return answer - 1;
+        return answer;
     }
 }
