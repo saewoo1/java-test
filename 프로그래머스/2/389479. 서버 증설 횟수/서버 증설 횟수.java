d5bb0fa8098a7520ccb6995f1bd9bc0619046c2b
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-03-24 22:37:19 +0900

    [level 2] Title: 서버 증설 횟수, Time: 5.07 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,21 +1,8 @@
-/*
-동시간대 이용자 m명이 늘어날 때마다 증설++
-m 미만이라면 증설 안함
-n * m명 이상 (n + 1) * m명 미만이라면
-최소 n대 증설 서버.
-
-1회 증설 시 k시간만큼 운영후 반납
-모든 게임 이용자가 게임을하기 위해서는, 최소 몇 번 증설해야하나?
-
-
-int limit = person / m;
-*/
 import java.util.*;
 
 class Server {
     int end;
     int count;
-    
     public Server(int end, int count) {
         this.end = end;
         this.count = count;
@@ -24,27 +11,20 @@ class Server {
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        int count = 0;
-        
-        PriorityQueue<Server> activeServers = 
-            new PriorityQueue<>((a, b) -> a.end - b.end);
-        
+        Queue<Server> activeServers = new ArrayDeque<>();
         for (int i = 0; i < 24; i++) {
-            // 현 시간대 증설해야하는 개수
             int need = players[i] / m;
             
-            // 시간 종료된 서버 회수
-            while (!activeServers.isEmpty() && activeServers.peek().end <= i) {
+            while (!activeServers.isEmpty() 
+                   && activeServers.peek().end <= i) {
                 activeServers.poll();
             }
-            int currentServerCount = 
-                activeServers.stream().mapToInt(s -> s.count).sum();
-            
+            int currentServerCount = activeServers.stream()
+                .mapToInt(e -> e.count).sum();
             if (currentServerCount < need) {
-                // 새로 증설해야하는 개수
-                int newServer = need - currentServerCount;
-                activeServers.add(new Server(i + k, newServer));
-                answer += newServer;
+                int newServerCount = need - currentServerCount;
+                activeServers.add(new Server(i + k, newServerCount));
+                answer += newServerCount;
             }
         }
         return answer;
