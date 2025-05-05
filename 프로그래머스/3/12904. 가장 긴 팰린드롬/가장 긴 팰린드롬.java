d03fe393a1ef546dfc4838fd1a37e071eff64faf
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-05-05 11:33:21 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 17.79 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -10,15 +10,17 @@ class Solution
             int evenLen = expandFromCenter(s, i, i + 1);
             int len = Math.max(oddLen, evenLen);
             
-            answer = Math.max(answer, len);
+            answer = Math.max(len, answer);
         }
-        
 
         return answer;
     }
     
     public int expandFromCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+        // left, right가 인덱스 범위를 벗어나지 않고, 두 char가 같은 값이라면
+        while (left >= 0 && right < s.length() 
+               && s.charAt(left) == s.charAt(right)) {
+            // 시작점부터 점점 퍼져나가면서 검수 진행
             left--;
             right++;
         }
