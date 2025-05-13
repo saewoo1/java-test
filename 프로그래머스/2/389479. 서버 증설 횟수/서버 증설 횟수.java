6fd3bc223c1eee7849b9acfc0d6ce6cf2fef04f4
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-05-13 13:01:07 +0900

    [level 2] Title: 서버 증설 횟수, Time: 3.30 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,30 +1,39 @@
 import java.util.*;
 
-class Server {
-    int end;
-    int count;
-    public Server(int end, int count) {
-        this.end = end;
-        this.count = count;
-    }
-}
 class Solution {
+    static class Server {
+        // 현 시점에서 돌고있는 대수
+        int count;
+        // 반환되는 시간
+        int end;
+        
+        public Server(int count, int end) {
+            this.count = count;
+            this.end = end;
+        }
+    }
+    
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        Queue<Server> activeServers = new ArrayDeque<>();
+        Queue<Server> currentServers = new ArrayDeque<>();
+        // 24시간동안 돌 때, 최소 몇 대의 서버가 더 증설되어야 할까?
         for (int i = 0; i < 24; i++) {
+            // 지금 당장 돌아야하는 서버의 대수
             int need = players[i] / m;
             
-            while (!activeServers.isEmpty() 
-                   && activeServers.peek().end <= i) {
-                activeServers.poll();
+            // 가동 끝난 애들은 모두 버린다
+            while (!currentServers.isEmpty()
+                  && currentServers.peek().end <= i) {
+                currentServers.poll();
             }
-            int currentServerCount = activeServers.stream()
-                .mapToInt(e -> e.count).sum();
-            if (currentServerCount < need) {
-                int newServerCount = need - currentServerCount;
-                activeServers.add(new Server(i + k, newServerCount));
-                answer += newServerCount;
+            // 회수 끝난 후, 그래서 지금 몇대 돌고 있음?
+            int currentCount = 
+                currentServers.stream().mapToInt(e -> e.count).sum();
+            // 만약 더 증설해야한다면
+            if (currentCount < need) {
+                int count = need - currentCount;
+                currentServers.add(new Server(count, i + k));
+                answer += count;
             }
         }
         return answer;
